bd8b99a473657a5e126eea5f3da8733475f333ec
Kayodé Da Cruz <kayodedacruz@MacBook-Pro-de-Kayode.local>
2026-01-01 15:49:13 +0100

    chore: chnager l'importation de la configuration Firebase

--- backend/config/schema.sql
+++ backend/config/schema.sql
@@ -72,11 +72,9 @@ CREATE TABLE IF NOT EXISTS utilisateurs (
     niveau_etude_id INTEGER REFERENCES niveau_etude(id),
     sexe utilisateurs_sexe_enum,
     telephone VARCHAR(50),
-    role utilisateurs_role_enum
+    role utilisateurs_role_enum,
+    fcm_token TEXT
 );
-
-ALTER TABLE utilisateurs
-ADD COLUMN IF NOT EXISTS fcm_token TEXT;
 
 
 -- ------------------------------
--- backend/src/firebase/firebase.module.ts
+++ backend/src/firebase/firebase.module.ts
@@ -1,11 +1,12 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FirebaseService } from './firebase.service';
+import { FirebaseConfig } from '../config/firebase.config';
 
 @Global()
 @Module({
   imports: [ConfigModule],
-  providers: [FirebaseService],
-  exports: [FirebaseService],
+  providers: [FirebaseService, FirebaseConfig],
+  exports: [FirebaseService, FirebaseConfig],
 })
 export class FirebaseModule { }
